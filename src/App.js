ee37978959e1a88e9055068839f00fba3d8bb07e
nyaruo <tanikawanko@gmail.com>
2022-01-21 21:52:13 +0900

    login & 画像生成画面の追加

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import { StateProvider } from './context/StateProvider'
+import { useRoutes } from 'react-router-dom'
+import { routePath } from './routes'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const element = useRoutes(routePath)
+  return <StateProvider>{element}</StateProvider>
 }
 
-export default App;
+export default App
---
+++ src/components/Layout.js
@@ -0,0 +1,69 @@
+import { useEffect, useState } from 'react'
+import { useNavigate, Link, Outlet, useLocation } from 'react-router-dom'
+import { useStateContext } from '../context/StateProvider'
+import { LogoutIcon } from '@heroicons/react/outline'
+import Modal from './Modal'
+
+const Layout = () => {
+    const { serviceName, isLogin } = useStateContext()
+    const navigate = useNavigate()
+    const [modalOn, setModalOn] = useState(false)
+    const location = useLocation()
+
+    const modal = () => {
+        setModalOn((prevState) => (prevState = true))
+    }
+    useEffect(() => {
+        if (!isLogin) navigate('/login')
+    }, [isLogin, navigate])
+
+    return (
+        <div className="flex items-center flex-col min-h-screen text-gray-600 font-mono">
+            <header className="flex items-center pl-8 h-14 bg-gray-600 w-screen">
+                <nav className="bg-gray-600 w-screen">
+                    <div className="flex justify-between">
+                        <div className="">
+                            <span className="font-semibold text-xl tracking-tight text-white">
+                                {serviceName}!!
+                            </span>
+                            <Link
+                                className="text-sm text-gray-200 hover:bg-gray-700 px-3 py-2 rounded"
+                                to="/"
+                            >
+                                Root
+                            </Link>
+                            <Link
+                                className="text-sm text-gray-200 hover:bg-gray-700 px-3 py-2 rounded"
+                                to="/component-a"
+                            >
+                                ComponentA
+                            </Link>
+                        </div>
+                        <div className="">
+                            <LogoutIcon
+                                className="h-8 w-10 text-gray-200 hover:bg-gray-700 px-1 mr-5 rounded"
+                                aria-hidden="true"
+                                onClick={() => {
+                                    modal()
+                                }}
+                            />
+                        </div>
+                    </div>
+                </nav>
+            </header>
+            <main>
+                <Outlet />
+                {modalOn ? <Modal setModalOn={setModalOn} /> : null}
+            </main>
+            {location.pathname === '/' ? null : <Link to="/">Top</Link>}
+            <footer className="bg-gray-400 w-screen bottom-0 h-14">
+                <div className="flex justify-center items-center">
+                    <p className="pt-3">{serviceName}@2021</p>
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+export default Layout
+
